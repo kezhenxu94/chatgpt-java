bc1aea01380ef9c7174f9716a7690b154dc1c5f6
kezhenxu94 <kezhenxu94@apache.org>
2023-12-01 14:24:49 +0800

    Ignore unknown property

--- chatgpt-java-sdk/src/main/java/io/github/kezhenxu94/chatgpt/internal/ChatGPTHttpResponse.java
+++ chatgpt-java-sdk/src/main/java/io/github/kezhenxu94/chatgpt/internal/ChatGPTHttpResponse.java
@@ -1,11 +1,13 @@
 package io.github.kezhenxu94.chatgpt.internal;
 
 import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import io.github.kezhenxu94.chatgpt.message.Message;
 
 import java.util.List;
 
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class ChatGPTHttpResponse {
   private final String id;
   private final String object;
